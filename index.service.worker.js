b5bfe180e1fda0925877d4f6ecd7625652387894
Michael Navazhylau <mikipux7@gmail.com>
2026-07-30 23:29:29 -0500

    Deploy direct touch hit-testing

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785471695|1623869';
+const CACHE_VERSION = '1785472125|1637658';
 /** @type {string} */
 const CACHE_PREFIX = 'PotionPuzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
